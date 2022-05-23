ffc5b3b6c935530257728e4ff0df9945c6c1b331
nataly <nvchulichkova@yandex.ru>
2022-05-23 21:22:51 +0300

    feat: add class FormValidator and PopupWithImage

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,14 @@
+import Popup from './Popup.js';
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupImg = this._popup.querySelector('.popup__image');
+    this._popupImgName = this._popup.querySelector('.popup__image-text');
+  }
+  open(link, name) {
+    this._popupImg.src = link;
+    this._popupImg.alt = name;
+    this._popupImgName.textContent = name;
+  }
+} 
