cc17fddf038c12fddc0ac31ddb42b05962833269
nataly <nvchulichkova@yandex.ru>
2022-05-30 18:25:29 +0300

    refactor:fix index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './pages/index.css';
 
-import { api } from './components/Api';
+import { Api } from './components/api';
 import UserInfo from './components/UserInfo';
 import Section from './components/Section';
-import Card from './components/Card';
+import Card from './components/card';
 import PopupWithForm from './components/PopupWithForm';
 import PopupWithImage from './components/PopupWithImage';
 import FormValidator from './components/FormValidator';
